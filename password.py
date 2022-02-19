7ab330a4698c1ff3e8119a94a5314213b7b19f57
Tanner <tannergames47@gmail.com>
2022-02-19 10:45:53 -0800

    3rd working, shortening

--- password.py
+++ password.py
@@ -1,23 +1,30 @@
 import random
 
 
-def random_password():
-    master_list = []
-    lower = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&;()*+,-./:;<=>?@[\]^_`{|}~ \"'
-    for i in lower:
-        master_list.append(i)
-    random.shuffle(master_list)
+# def random_password():
+#     master_list = []
+#     lower = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&;()*+,-./:;<=>?@[\]^_`{|}~ "'
+#     for i in lower:
+#         master_list.append(i)
+#     random.shuffle(master_list)
 
-    return master_list
-
-
-your_password = int(input('How many characters would you like your password to be? '))
+#     return master_list
 
 
-output_password = ''
+# your_password = int(input("How many characters would you like your password to be? "))
+
+
+# output_password = ''
+# for i in range(your_password):
+#     save = random_password()
+#     output_password += save[0]
+
+your_password = int(input("How many characters would you like your password to be? "))
+output_password = ""
+digits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#%+/:=?@_"
+
 for i in range(your_password):
-    save = random_password()
-    output_password += save[0]
-
+    save = random.choice(digits)
+    output_password += save
 
 print(output_password)
